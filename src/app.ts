cd489cd786eb109b05156b5f40c6ddd25eaa8f55
tanver909 <tanveermahidi@gmial.com>
2025-09-28 21:41:58 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 
 import express, { Request, Response } from "express"
 import { router } from "./app/routes";
+import cors from "cors";
 import { globalErrorHandler } from "./app/middleware/globalerrorhandler";
 import notFounde from "./app/middleware/notFounde";
 const app = express();
@@ -9,7 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-
+app.use(cors({
+  origin: ["http://localhost:3000", "https://your-frontend-domain.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
